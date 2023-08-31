607afaae5696c6ac9ae9e0c68d1c336963f6badd
BLam1226 <brendanlam0@gmail.com>
2023-08-31 02:10:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection"); // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Use apiRoutes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
